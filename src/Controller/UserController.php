e58177764ef1f666b3a78272bb8e8ef194797c36
JosephDeFacto <josip.mareljic@gmail.com>
2022-03-01 23:48:24 +0100

    added user registration and logout route

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,12 +2,46 @@
 
 namespace App\Controller;
 
-use App\Repository\OrderRepository;
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
+    /**
+     * @Route("user/register", name="user_register")
+     */
+    public function register(Request $request, UserPasswordHasherInterface $passwordHasher, ManagerRegistry $registry)
+    {
+        $user = new User();
 
+        $form = $this->createForm(RegistrationFormType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $password = $passwordHasher->hashPassword($user, $user->getPassword());
+            $user->setPassword($password);
+            $user->setRoles((array)'ROLE_ADMIN');
+
+            $em = $registry->getManager();
+
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('user/register.html.twig', [
+            'form' => $form->createView(),
+        ]);
+
+
+    }
 }
